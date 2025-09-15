9b64abaa4686a90d2a3c5e590373e53f81050281
hughesg14 <hughesg1415@gmail.com>
2025-09-15 22:14:56 +0100

    Fully furnished bot with sys implemented

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from stats import count_ch
 from stats import sort_items
 import sys
 
+if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 def get_book_text(filepath):
     with open(filepath) as fp:
         file_contents = fp.read()
     return file_contents
 
 def main():
-
-    if len(sys.argv) != 2:
-        sys.exit(1)
-        print("Usage: python3 main.py <path_to_book>")
 
     contents = get_book_text(sys.argv[1])
     print(contents)
